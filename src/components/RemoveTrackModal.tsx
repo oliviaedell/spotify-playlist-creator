b090024fbc56bfcd9e265c52bd15265f366d650e
olivia edell <47330607+oliviaedell@users.noreply.github.com>
2021-09-20 02:02:42 -0400

    create React component remove track modal

---
+++ src/components/RemoveTrackModal.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react"
+import Modal from "react-bootstrap/Modal"
+import Button from "react-bootstrap/Button"
+import type { Track, Playlist } from "../Types"
+
+import "./NewPlaylistModal.css"
+
+type modalProps = {
+  showModal: boolean
+  setShowModal: (show: boolean) => void
+  selectedTrackID: number
+  playlist: Playlist
+}
+
+const RemoveTrackModal = ({
+  showModal,
+  setShowModal,
+  selectedTrackID,
+  playlist,
+}: modalProps) => {
+  const [storedPlaylists, setStoredPlaylists] = useState<Array<Playlist>>([])
+  const [selectedTrack, setSelectedTrack] = useState<Track>()
+  const [removed, setRemoved] = useState<boolean>(false)
+
+  useEffect(() => {
+    setStoredPlaylists(JSON.parse(localStorage.playlists))
+    setSelectedTrack(playlist.tracks[selectedTrackID])
+  }, [])
+
+  // const handleShowModal = () => {
+  //   setShowModal(!showModal)
+  // }
+  const handleConfirmRemove = () => {
+    if (storedPlaylists && playlist.tracks) {
+      playlist.tracks.splice(selectedTrackID, 1)
+      storedPlaylists[playlist.id].tracks = playlist.tracks
+      setRemoved(true)
+    }
+  }
+  useEffect(() => {
+    if (removed) {
+      localStorage.setItem("playlists", JSON.stringify(storedPlaylists))
+      setRemoved(false)
+      setShowModal(false)
+    }
+  }, [removed])
+
+  return (
+    <Modal centered show={showModal} onHide={() => setShowModal(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>Remove Song from Playlist</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          Remove {selectedTrack ? selectedTrack.item.name : "null"} by
+          {selectedTrack
+            ? selectedTrack.item.artists[0].name
+            : "null"} from {playlist.name}?
+        </p>
+        <Button variant="primary" onClick={handleConfirmRemove}>
+          Confirm
+        </Button>
+        <Button variant="secondary" onClick={() => setShowModal(false)}>
+          Cancel
+        </Button>
+      </Modal.Body>
+    </Modal>
+  )
+}
+
+export default RemoveTrackModal
--- src/pages/PlaylistDetailsPage.tsx
+++ src/pages/PlaylistDetailsPage.tsx
@@ -1,9 +1,10 @@
 import Button from "react-bootstrap/Button"
 import { useEffect, useState } from "react"
-import { ListGroup, Modal } from "react-bootstrap"
+import { ListGroup } from "react-bootstrap"
 import type { Playlist } from "../Types"
 
 import "./PlaylistDetailsPage.css"
+import RemoveTrackModal from "../components/RemoveTrackModal"
 
 type detailsProps = {
   playlist_id: number
@@ -12,28 +13,26 @@ type detailsProps = {
 const PlaylistDetails = ({ playlist_id }: detailsProps) => {
   const [playlist, setPlaylist] = useState<Playlist>()
   const [showModal, setShowModal] = useState<boolean>(false)
+  const [trackID, setTrackID] = useState<number>()
 
   useEffect(() => {
-    console.log(playlist_id || playlist_id === 0)
     if (playlist_id || playlist_id === 0) {
       setPlaylist(JSON.parse(localStorage.playlists)[playlist_id])
     }
   }, [])
 
-  const handleShowModal = () => {
-    setShowModal(!showModal)
+  const handleModalClick = (i: number) => {
+    setTrackID(i)
+    setShowModal(true)
   }
-  const handleConfirmRemove = () => {
-    //TODO: remove from playlist and update local storage
-    setShowModal(false)
-  }
+
   return playlist ? (
     <>
       <h1>Playlist Details</h1>
       <h2>{playlist.name}</h2>
       <ListGroup className="songList">
         {playlist.tracks ? (
-          playlist.tracks.items.map((track, i) => (
+          playlist.tracks.map((track, i) => (
             <div className="d-flex">
               <ListGroup.Item className="trackInfo">
                 {/* TODO album cover */}
@@ -43,33 +42,26 @@ const PlaylistDetails = ({ playlist_id }: detailsProps) => {
                 </p>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button onClick={handleShowModal}>X</Button>
+                <Button onClick={() => handleModalClick(i)}>X</Button>
               </ListGroup.Item>
-              <Modal centered show={showModal} onHide={handleShowModal}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Remove Song from Playlist</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <p>
-                    Remove {track.item.name} by {track.item.artists[0].name}{" "}
-                    from {playlist.name}?
-                  </p>
-                  <Button variant="primary" onClick={handleConfirmRemove}>
-                    Confirm
-                  </Button>
-                  <Button variant="secondary" onClick={handleShowModal}>
-                    Cancel
-                  </Button>
-                </Modal.Body>
-              </Modal>
             </div>
           ))
         ) : (
           <>
-            <h1>no playlist </h1>
+            <h1>no tracks </h1>
           </>
         )}
       </ListGroup>
+      {trackID ? (
+        <RemoveTrackModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          selectedTrackID={trackID}
+          playlist={playlist}
+        />
+      ) : (
+        <></>
+      )}
     </>
   ) : (
     <></>
